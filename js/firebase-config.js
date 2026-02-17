31eb64699961fc383d5c9fd011b509bd2763ce7a
kino-Mk <daichitamai0531@gmail.com>
2026-02-17 16:26:23 +0900

    Add files via upload

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,25 +1,25 @@
 // TODO: 2つのFirebaseプロジェクトの設定に置き換えてください
 
 // 1. 本番環境 (GitHub Pages用)
-const firebaseConfig = {
-  apiKey: "AIzaSyBGvGIeD2rlK4FGZeVX10IUrbNKCbkdVaY",
-  authDomain: "kintai-f2c7f.firebaseapp.com",
-  projectId: "kintai-f2c7f",
-  storageBucket: "kintai-f2c7f.firebasestorage.app",
-  messagingSenderId: "452090687947",
-  appId: "1:452090687947:web:d794ad91948756145ac64e",
-  measurementId: "G-XRYQKKGGM0"
+const firebaseConfigProd = {
+    apiKey: "AIzaSyBGvGIeD2rlK4FGZeVX10IUrbNKCbkdVaY",
+    authDomain: "kintai-f2c7f.firebaseapp.com",
+    projectId: "kintai-f2c7f",
+    storageBucket: "kintai-f2c7f.firebasestorage.app",
+    messagingSenderId: "452090687947",
+    appId: "1:452090687947:web:d794ad91948756145ac64e",
+    measurementId: "G-XRYQKKGGM0"
 };
 
 // 2. テスト環境 (ローカル開発用: localhost / 127.0.0.1)
-const firebaseConfig_test = {
-  apiKey: "AIzaSyDoQLL35HsODD1wR4DTAEbvBFGXlo_OG34",
-  authDomain: "kintai-test-server.firebaseapp.com",
-  projectId: "kintai-test-server",
-  storageBucket: "kintai-test-server.firebasestorage.app",
-  messagingSenderId: "653500342831",
-  appId: "1:653500342831:web:f16e66aa2e815e1248dcda",
-  measurementId: "G-HMMLHVD434"
+const firebaseConfigDev = {
+    apiKey: "AIzaSyDoQLL35HsODD1wR4DTAEbvBFGXlo_OG34",
+    authDomain: "kintai-test-server.firebaseapp.com",
+    projectId: "kintai-test-server",
+    storageBucket: "kintai-test-server.firebasestorage.app",
+    messagingSenderId: "653500342831",
+    appId: "1:653500342831:web:f16e66aa2e815e1248dcda",
+    measurementId: "G-HMMLHVD434"
 };
 // 環境判定ロジック
 const hostname = window.location.hostname;
